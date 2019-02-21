a4eef1ff6ee769d4a09d378cd3a46f3d0d416de9
Peter Lee <peter.lee230597@gmail.com>
2019-02-21 18:38:48 +0800

    first commit for controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,29 @@
 class ProfilesController < ApplicationController
 
   def index
-    @mentors = Profile.where(is_mentor: true)
+    @profiles = Profile.where(is_mentor: true)
+    if params[:query].present?
+      sql_query = " \
+        spaces.name @@ :query \
+        OR spaces.location @@ :query \
+        AND spaces.category @@ :category \
+      "
+      @profiles = Profile.where(sql_query, category: "%#{params[:category]}%", query: "%#{params[:query]}%")
+    else
+      @profiles = Profile.all
+    end
   end
 
   def new
+    #the third page in the signup form
   end
 
   def create
+    #the third page in the signup form
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def update
