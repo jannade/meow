c290340c7dd4005e179cf9215ba7c5ef5a6e8ac5
Ashwin Vijendra <ack4and@gmail.com>
2019-02-21 13:19:28 +0800

    removed string column in users table which was accidentally added

--- db/migrate/20190221035106_add_photo_to_users.rb
+++ db/migrate/20190221035106_add_photo_to_users.rb
@@ -1,6 +1,5 @@
 class AddPhotoToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :photo, :string
-    add_column :users, :string, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2019_02_21_035106) do
 
   # These are extensions that must be enabled in order to support this database
@@ -52,7 +53,6 @@ ActiveRecord::Schema.define(version: 2019_02_21_035106) do
     t.string "company"
     t.string "job_title"
     t.string "photo"
-    t.string "string"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
