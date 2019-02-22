7056f9606607ea803faaf07f4c32c0b7af646157
Peter Lee <peter.lee230597@gmail.com>
2019-02-22 12:51:12 +0800

    created function for percentage

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,13 @@
 class ProfilesController < ApplicationController
-
   def index
-    @profiles = Profile.where(is_mentor: true)
-    if params[:query].present?
-      sql_query = " \
-        spaces.name @@ :query \
-        OR spaces.location @@ :query \
-        AND spaces.category @@ :category \
-      "
-      @profiles = Profile.where(sql_query, category: "%#{params[:category]}%", query: "%#{params[:query]}%")
-    else
-      @profiles = Profile.all
-    end
+
+    # First time user go to browse page, they will see their recommended mentors
+    # if professional interest is all and personal interest is not all, show mentors with any professional interest with the filtered personal interest
+    # if personal interest is all and professional interest is not all, show mentors with any personal interest with the filtered professional interest
+    # if personal interest and professional interest is all show all mentors
+
+    # matchmaking algorithm
+    # match mentors and user based on the percentage in their interest match
   end
 
   def new
@@ -31,4 +27,19 @@ class ProfilesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def percentage(mentor_interest, mentee_interest)
+    matched_interest = []
+    mentor_interest.each do |element|
+      matched_interest << if element.include? mentee_interest
+      end
+    end
+    (matched_interest.count/mentee_array.count)*100
+  end
+
+  # create a list in the browser page
+  # the higher the number of percentage, put the mentor in the top for recommended mentor
+  # create a button for clear filter
 end
