a223ae5fa41c5cf4b124b91830ea47cd56bf338d
Ashwin Vijendra <ack4and@gmail.com>
2019-02-22 17:29:47 +0800

    removed commented line

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,4 @@ Rails.application.routes.draw do
 
   resources :messages, except: [:edit, :update]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
