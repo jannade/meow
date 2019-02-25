aab223e3634b71b6d21134c96d020d6ac2bde16a
Peter Lee <peter.lee230597@gmail.com>
2019-02-25 17:36:03 +0800

    Finished the filter and algorithm

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,8 @@
 class ProfilesController < ApplicationController
   def index
-    @profiles = find_mentor_by_interests((params[:professional_interests]), (params[:personal_interests]))
-    @user = current_user
     @recommended_profiles = recommended_profiles
+    @user = current_user
+    @profiles = find_mentor_by_interests((params[:professional_interests]), (params[:personal_interests])).uniq
   end
 
   def new
@@ -46,51 +46,13 @@ class ProfilesController < ApplicationController
 
   def find_mentor_by_interests(first_interest, second_interest)
     if first_interest == "All" && second_interest != "All"
-      Profile.joins(:interests).where(interests: { name: second_interest}).where(is_mentor: true)
+      Profile.joins(:interests).where(interests: { name: "#{second_interest}"}).where(is_mentor: true)
     elsif first_interest != "All" && second_interest == "All"
-      Profile.joins(:interests).where(interests: { name: first_interest}).where(is_mentor: true)
+      Profile.joins(:interests).where(interests: { name: "#{first_interest}"}).where(is_mentor: true)
     elsif first_interest == "All" && second_interest == "All"
       Profile.all.where(is_mentor: true)
     else
-      Profile.joins(:interests).where(interests: { name: first_interest}).or(Profile.joins(:interests).where(interests: {name: second_interest })).where(is_mentor: true)
+      Profile.joins(:interests).where(interests: { name: "#{first_interest}"}).or(Profile.joins(:interests).where(interests: {name: "#{second_interest}" })).where(is_mentor: true)
     end
   end
 end
-
-
-
-
-    # matched_interest = []
-    # recommended = []
-    # Profile.where(is_mentor: true).each do |profile|
-    #   User.find(Profile.where(is_mentor:true).pluck(:user_id)).each do |mentoruser|
-    #     mentoruser.interests.each do |interest|
-    #       matched_interest << interest.name if peter.interests.include? interest
-    #     end
-    #   end
-    # match_percentage = (matched_interest.count.to_f / peter.interests.count.to_f) * 100
-    # recommended << profile if match_percentage >= 80
-    # end
-    # recommended
-
-
-
-    #     ashwin.interests.each do |interest|
-    #       matched_interest << interest.name if peter.interests.include? interest
-    #     end
-    # match_percentage = (matched_interest.count.to_f / peter.interests.count.to_f) * 100
-
-    #         george.interests.each do |interest|
-    #       matched_interest << interest.name if peter.interests.include? interest
-    #     end
-    # match_percentage = (matched_interest.count.to_f / peter.interests.count.to_f) * 100
-
-
-
-
-
-
-
-
-
-
