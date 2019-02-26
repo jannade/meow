cb8919b34eb3aee2ba0cf3799e5a165de1fdb458
Ashwin Vijendra <ack4and@gmail.com>
2019-02-26 15:19:45 +0800

    mentor and mentee page correctly setup(yet to get photo working)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,10 @@
 class ProfilesController < ApplicationController
   def index
-    # @recommended_profiles = recommended_profiles
-    # @user = current_user
+   # @recommended_profiles = recommended_profiles
+   # @user = current_user
 
     if params[:professional_interests].present? || params[:personal_interests].present?
-      @profiles = find_mentor_by_interests((params[:professional_interests]), (params[:personal_interests]))
+      @profiles = find_mentor_by_interests(params[:professional_interests], params[:personal_interests])
     else
       @profiles = recommended_profiles
     end
@@ -21,13 +21,20 @@ class ProfilesController < ApplicationController
     @user = current_user
     @profile = Profile.new(profile_params)
     @profile.user = @user
-    @user.company = params[:user][:company]
-    @user.job_title = params[:user][:job_title]
-    @user.photo = params[:user][:photo]
-    @user.update(user_params)
+
+    unless @user.company.present? && @user.job_title.present?
+      @user.company = params[:user][:company]
+      @user.job_title = params[:user][:job_title]
+      @user.photo = params[:user][:photo]
+      @user.update(user_params)
+    end
+
+    is_mentor_flow = params[:profile][:is_mentor]
+
+    onboard_redirect = is_mentor_flow ? new_mentee_path : mentors_path
 
     if @profile.save
-      redirect_to new_mentee_path
+      redirect_to onboard_redirect
     else
       render :new
     end
@@ -66,9 +73,9 @@ class ProfilesController < ApplicationController
       profile.user.interests.each do |interest|
         matched_interest << interest if current_user.interests.include? interest
       end
-        match_percentage = 0
-        match_percentage = (matched_interest.count.to_f / current_user.interests.count.to_f) * 100
-        recommended << profile if match_percentage >= 70
+      match_percentage = 0
+      match_percentage = (matched_interest.count.to_f / current_user.interests.count.to_f) * 100
+      recommended << profile if match_percentage >= 70
     end
     recommended
   end
@@ -77,22 +84,24 @@ class ProfilesController < ApplicationController
     params.require(:user).permit(:company, :job_title, :photo)
   end
 
+  def profile_params
+    params.require(:profile).permit(:is_mentor, :description)
+  end
+
   def add_profile_interests(interest_ids)
     interest_ids.each do |id|
       interest = Interest.find(id)
       ProfileInterest.create!(profile: @profile, interest: interest)
-      matched_interest = []
     end
-    recommended
   end
 
   private
 
   def find_mentor_by_interests(first_interest, second_interest)
     if first_interest == "All" && second_interest != "All"
-      Profile.joins(:interests).where(interests: { name: "#{second_interest}"}).where(is_mentor: true)
+      Profile.joins(:interests).where(interests: { name: " #{second_interest}" }).where(is_mentor: true)
     elsif first_interest != "All" && second_interest == "All"
-      Profile.joins(:interests).where(interests: { name: "#{first_interest}"}).where(is_mentor: true)
+      Profile.joins(:interests).where(interests: { name: " #{first_interest}" }).where(is_mentor: true)
     elsif first_interest == "All" && second_interest == "All"
       Profile.all.where(is_mentor: true)
     else
@@ -101,8 +110,6 @@ class ProfilesController < ApplicationController
     end
   end
 end
-
-
 
 
 
